63796b9004e8834df92fae73ac6811a68f54bf6b
bordace <erik.bordac@gmail.com>
2023-10-13 22:39:08 +0200

    resolve conflicts

--- TeamProject/Assets/script/NPC_Script.cs
+++ TeamProject/Assets/script/NPC_Script.cs
@@ -11,56 +11,6 @@ public class NPC_Script : MonoBehaviour
 	public int CorrectAnswer;
 
 	public GameObject TalkButtonHint;
-	public GameLogicScript GameLogicScript;
-
-	private bool _isTalkable;
-
-	private void Start()
-	{
-		TalkButtonHint.SetActive(false);
-	}
-
-	private void Update()
-	{
-		if (_isTalkable && Input.GetKeyDown(KeyCode.E))
-		{
-			GameLogicScript.StartDialog(Question,
-										new string[]
-										{
-											PossibleAnswer1,
-											PossibleAnswer2,
-											PossibleAnswer3,
-										},
-										CorrectAnswer);
-		}
-	}
-
-	private void OnTriggerEnter2D(Collider2D collision)
-	{
-		TalkButtonHint.SetActive(true);
-		_isTalkable = true;
-	}
-
-	private void OnTriggerExit2D(Collider2D collision)
-	{
-		TalkButtonHint.SetActive(false);
-		_isTalkable = false;
-	}
-}
-=======
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class NPC_Script : MonoBehaviour
-{
-	public string Question;
-	public string PossibleAnswer1;
-	public string PossibleAnswer2;
-	public string PossibleAnswer3;
-	public int CorrectAnswer;
-
-	public GameObject TalkButtonHint;
 	public DialogManager GameLogicScript;
 
 	private bool _isTalkable;
@@ -97,4 +47,3 @@ public class NPC_Script : MonoBehaviour
 		_isTalkable = false;
 	}
 }
->>>>>>> main:TeamProject/Assets/NPC_Script.cs
